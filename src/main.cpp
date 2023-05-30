db7f649a52151c40a1358b4316470dfffbe7aea0
陈阳 <1337968347@qq.com>
2023-05-30 19:05:47 +0800

    :star: ...

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,31 @@
+#include <iostream>
+#include <fstream>
 #include "./core/pbrt.h"
 #include "./core/transform.cpp"
 
 int main(int argc, const char** argv) {
-    Matrix4x4 m1;
-    m1 = Inverse(m1);
-    FILE* fp;
-    fp = fopen("example.txt", "w");
-    m1.Print(fp);
+    
+    
+        const int width = 640;
+    const int height = 480;
+    const int max_color_value = 255;
+
+    std::ofstream image("image.ppm");
+    image << "P3" << std::endl;
+    image << width << " " << height << std::endl;
+    image << max_color_value << std::endl;
+
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            int red = x % (max_color_value + 1);
+            int green = y % (max_color_value + 1);
+            int blue = (x + y) % (max_color_value + 1);
+            image << red << " " << green << " " << blue << " ";
+        }
+        image << std::endl;
+    }
+
+    image.close();
+
     return 0;
 }
